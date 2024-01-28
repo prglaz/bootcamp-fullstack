5e6df48f5e5a8426bcf822bfe103b6e544087df5
Pablo Glaz <prglaz@gmail.com>
2024-01-28 13:44:40 -0300

    desafio 15 - alternativa con props

--- desafio15/src/App.jsx
+++ desafio15/src/App.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
 import ClimaCiudad from "./ClimaCiudad";
 
 const App = () => {
+    const [inputCity, setInputCity] = useState("");
+    const [cityToSearch, setCityToSearch] = useState("");
+
+    const handleOnChangeInputCity = (e) => {
+        // Actualiza el valor de input mientras se escribe
+        setInputCity(e.target.value);
+    };
+
+    const handleOnClickGetTemp = (e) => {
+        e.preventDefault();
+        setCityToSearch(inputCity);
+    };
+
     return (
         <>
-            <ClimaCiudad />
+            <main>
+                <h1>Temperatura</h1>
+                <div>
+                    <form>
+                        <input
+                            type="text"
+                            id="City"
+                            value={inputCity}
+                            onChange={(e) => handleOnChangeInputCity(e)}
+                            required />
+                        <button onClick={(e) => handleOnClickGetTemp(e)}>Obtener temperatura</button>
+                    </form>
+                </div>
+                <ClimaCiudad cityToSearch={cityToSearch} />
+            </main>
         </>
     );
 };
